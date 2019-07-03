ddd7160858c16d838393409388f0b321514a7efb
trinidadMC <triny.23.93@gmail.com>
2019-07-03 11:45:52 -0500

    arreglo del css

--- src/main.js
+++ src/main.js
@@ -1,41 +1,41 @@
 /*DECLRANDO LOS BOTONES DEL LOGIN Y DAR(LE FUNCIONABILIDAD*/
-const loginbox = document.getElementById("loginbox");
-const login = document.getElementById("login");
-const password = document.getElementById("contraseña");
-const buttonUno = document.getElementById("btn1");
-const error = document.getElementById("error");
-const interfaz2 = document.getElementById("interfaz2");
-let contador = 0;
-buttonUno.addEventListener("click", () => {
-  if (login.value && password.value === "LABORATORIA") {
-    loginbox.classList.add("hide");
-    interfaz2.classList.remove("hide");
-
+  const loginbox = document.getElementById("loginbox");
+  const login = document.getElementById("login");
+  const password = document.getElementById("contraseña");
+  const buttonUno = document.getElementById("btn1");
+  const error = document.getElementById("error");
+  const interfaz2 = document.getElementById("interfaz2");
+  let contador = 0;
+  buttonUno.addEventListener("click", () => {
+    if (login.value && password.value === "LABORATORIA") {
+      loginbox.classList.add("hide");
+      interfaz2.classList.remove("hide");
+  
+    }
+    else if (contador == 2) {
+      error.innerHTML = "intentaste 3 veces ya no puedes entrar a la pagina";
+    }
+    else {
+      contador++
+      error.innerHTML = "contraseña incorrecta";
+    }
+  });
+  /*LLAMANDO A MI DATA POKEMON DESDE DATA JS */
+  const contenedor = document.getElementById("contenedor");
+  const pokemones = POKEMON.pokemon;
+  const mostrardata = (pokemon) =>{
+  let mostrar =' ';
+  for( let i = 0; i < pokemon.length; i++) {
+    let llamar = `
+    <div class='mostrar'>
+      <img src="${pokemon[i].img}"/>
+      <p>nombre=${pokemon[i].name}</p>
+    </div>`;
+    mostrar += llamar;
   }
-  else if (contador == 2) {
-    error.innerHTML = "intentaste 3 veces ya no puedes entrar a la pagina";
-  }
-  else {
-    contador++
-    error.innerHTML = "contraseña incorrecta";
-  }
-});
-/*LLAMANDO A MI DATA POKEMON DESDE DATA JS */
-const contenedor = document.getElementById("contenedor");
-const pokemones = POKEMON.pokemon;
-const mostrardata = (pokemon) =>{
-let mostrar =' ';
-for( let i = 0; i < pokemon.length; i++) {
-  let llamar = `
-  <div>
-    <img src="${pokemon[i].img}"/>
-    <p>nombre=${pokemon[i].name}</p>
-  </div>`;
-  mostrar += llamar;
-}
-return mostrar;
-};
-contenedor.innerHTML = mostrardata(pokemones);
+  return mostrar;
+  };
+  contenedor.innerHTML = mostrardata(pokemones)
 
 
 
