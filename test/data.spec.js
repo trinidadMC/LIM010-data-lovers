5d42644bd6f9935cf88bf4bcc909dca0fe667247
trinidadMC <triny.23.93@gmail.com>
2019-07-23 11:31:30 -0500

    arreglos del readme

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,95 +1,4 @@
 global.window = global;
-<<<<<<< HEAD
-require('../src/data');
-require('./data.spec.js');
-
-const dataPokemon = [{
-  "id": 1,
-  "num": "001",
-  "name": "Bulbasaur",
-  "img": "http://www.serebii.net/pokemongo/pokemon/001.png",
-  "type": [
-    "Grass",
-    "Poison"
-  ],
-  "height": "0.71 m",
-  "weight": "6.9 kg",
-  "candy": "Bulbasaur Candy",
-  "candy_count": 25,
-  "egg": "2 km",
-  "spawn_chance": 0.69,
-  "avg_spawns": 69,
-  "spawn_time": "20:00",
-  "multipliers": [1.58],
-  "weaknesses": [
-    "Fire",
-    "Ice",
-    "Flying",
-    "Psychic"
-  ],
-  "next_evolution": [{
-    "num": "002",
-    "name": "Ivysaur"
-  }, {
-    "num": "003",
-    "name": "Venusaur"
-  }],
-
-  "id": 23,
-    "num": "023",
-    "name": "Ekans",
-    "img": "http://www.serebii.net/pokemongo/pokemon/023.png",
-    "type": [
-      "Poison"
-    ],
-    "height": "2.01 m",
-    "weight": "6.9 kg",
-    "candy": "Ekans Candy",
-    "candy_count": 50,
-    "egg": "5 km",
-    "spawn_chance": 2.27,
-    "avg_spawns": 227,
-    "spawn_time": "12:20",
-    "multipliers": [
-      2.21,
-      2.27
-    ],
-    "weaknesses": [
-      "Ground",
-      "Psychic"
-    ],
-    "next_evolution": [{
-      "num": "024",
-      "name": "Arbok"
-    }]
-  }, {
-    "id": 106,
-    "num": "106",
-    "name": "Hitmonlee",
-    "img": "http://www.serebii.net/pokemongo/pokemon/106.png",
-    "type": [
-      "Fighting"
-    ],
-    "height": "1.50 m",
-    "weight": "49.8 kg",
-    "candy": "None",
-    "egg": "10 km",
-    "spawn_chance": 0.02,
-    "avg_spawns": 2,
-    "spawn_time": "03:59",
-    "multipliers": null,
-    "weaknesses": [
-      "Flying",
-      "Psychic",
-      "Fairy"
-    ]
-}]
-
-
-describe('azordena', () => {
-  it('debería ser una función', () => {
-    expect(typeof pokemon.azordena).toBe('function');
-=======
 // global.assert = require('chai').assert;
 require('../src/data');
 require('./data.spec.js');
@@ -146,20 +55,12 @@ describe('azordena', () => {
 describe('poketypos', () => {
   it('debería ser una función', () => {
     expect(typeof pokemon.poketypos).toEqual('function');
->>>>>>> f20cd6cbef70d461c91741e1856c95ad088a362d
   });
   it('debería retornar Charmander', () => {
     expect(pokemon.poketypos(dataPokemonTest, 'Fire')[0].name).toEqual('Charmander');
   });
 });
 
-<<<<<<< HEAD
-  it('debería retornar un array de objetos ordenado en forma alfabetica ', () => {
-    expect(azordena()[0].name).toEqual('Bulbasaur','Ekans');
-  });
-
-})
-=======
 describe('ordenpoder', () => {
   it('debería ser una función', () => {
     expect(typeof pokemon.ordenpoder).toEqual('function');
@@ -195,4 +96,3 @@ describe('huevos', () => {
     expect(pokemon.huevos(dataPokemonTest, '10 km')[0].egg).toEqual('10 km');
   });
 });
->>>>>>> f20cd6cbef70d461c91741e1856c95ad088a362d
